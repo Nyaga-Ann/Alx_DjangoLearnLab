c3e567ac103b112bfb03b2925be70314a2d5863e
Ann Nyaga <137498254+Nyaga-Ann@users.noreply.github.com>
2025-07-26 23:38:35 +0300

    Add custom book permissions and secure views with permission_required

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic.detail import DetailView
-from .models import Book
+from .models import Book, Author
 from .models import Library 
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView, LogoutView
@@ -9,6 +9,8 @@ from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.http import HttpResponse
 from .models import UserProfile
+from django.contrib.auth.decorators import permission_required
+
 
 # Function-based view
 def list_books(request):
@@ -67,3 +69,34 @@ def librarian_view(request):
 @user_passes_test(is_member)
 def member_view(request):
     return render(request, "relationship_app/member_view.html")
+
+@permission_required("relationship_app.can_add_book")
+def add_book(request):
+    if request.method == "POST":
+        title = request.POST.get("title")
+        author_id = request.POST.get("author")
+        author = get_object_or_404(Author, id=author_id)
+        Book.objects.create(title=title, author=author)
+        return HttpResponseRedirect("/books/")
+    authors = Author.objects.all()
+    return render(request, "relationship_app/add_book.html", {"authors": authors})
+
+@permission_required("relationship_app.can_change_book")
+def edit_book(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == "POST":
+        book.title = request.POST.get("title")
+        author_id = request.POST.get("author")
+        book.author = get_object_or_404(Author, id=author_id)
+        book.save()
+        return HttpResponseRedirect("/books/")
+    authors = Author.objects.all()
+    return render(request, "relationship_app/edit_book.html", {"book": book, "authors": authors})
+
+@permission_required("relationship_app.can_delete_book")
+def delete_book(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == "POST":
+        book.delete()
+        return HttpResponseRedirect("/books/")
+    return render(request, "relationship_app/delete_book.html", {"book": book})
