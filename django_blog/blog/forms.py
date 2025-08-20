bf7156b4bf6c99bbce1d5cf5105890356042d0d0
Ann Nyaga <137498254+Nyaga-Ann@users.noreply.github.com>
2025-08-20 22:23:14 +0300

    update

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,7 +1,8 @@
 # blog/forms.py
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User  # needed for checks
+from django.contrib.auth.models import User  
+from .models import Post
 
 class RegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -14,3 +15,8 @@ class ProfileForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ("first_name", "last_name", "email")
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ["title", "content"]
