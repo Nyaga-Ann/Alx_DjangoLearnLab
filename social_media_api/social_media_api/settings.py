03f6a07a0099b3315da160e06fbb3209e56173e4
Ann Nyaga <137498254+Nyaga-Ann@users.noreply.github.com>
2025-08-24 16:50:31 +0300

    Addedpost

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
     # Local apps
     'accounts',
+    'posts',
 ]
 
 MIDDLEWARE = [
@@ -80,9 +81,12 @@ AUTH_USER_MODEL = 'accounts.User'
 
 # DRF settings
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
-    ]
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 5,
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.TokenAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+    ],
 }
 
 # Database
